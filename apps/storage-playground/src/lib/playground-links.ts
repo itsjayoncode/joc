dd6d7dc57e922d38d1ee9c795785dc0e2f6218b4
Jay <jaymago.it@gmail.com>
2026-07-22 04:36:43 +0800

    release: storage 0.1.0 and doc update

---
+++ apps/storage-playground/src/lib/playground-links.ts
@@ -0,0 +1,8 @@
+export const STORAGE_DOCS_URL: string =
+  (import.meta.env.VITE_STORAGE_DOCS_URL as string | undefined) ??
+  "https://itsjayoncode.github.io/joc/packages/storage/";
+
+export const STORAGE_NPM_URL = "https://www.npmjs.com/package/@jayoncode/storage";
+
+export const STORAGE_REPO_URL =
+  "https://github.com/itsjayoncode/joc/tree/master/packages/storage";
